7bccbf9e4bf9ab14e2d4baf8983fa7bea6995fa2
AliSepar <alisabah.separ1@gmail.com>
2024-04-04 17:21:19 +0200

    completing the panales and makeing them functional with javascript

---
+++ newsletter-sign-up-with-success-message-main/app.js
@@ -0,0 +1,41 @@
+
+let btn = document.querySelector(".btn");
+let successBtn = document.querySelector('.s-btn');
+
+let contentClass = document.querySelector('.content');
+let imageClass = document.querySelector('.image');
+let errormessage=document.querySelector('.errormasg');
+let successcard = document.querySelector('.sucsses-card');
+let clintEmail = document.querySelector("#clint-email");
+
+btn.addEventListener("click",function(){
+let getEmail = document.querySelector('.email');
+
+let emailValue= getEmail.value;
+if(validateEmail(emailValue)){
+    // textbox 
+    errormessage.classList.add("hide");
+    getEmail.classList.remove("errorinbox");
+
+    // move to other panal
+    contentClass.classList.add("hide"); //hiding the content div
+    imageClass.classList.add("hide"); //hiding the image div
+
+    successcard.classList.remove("hide"); //removing the hide class from div
+    successcard.classList.add("show");//adding the show class to the div
+    clintEmail.textContent=getEmail.value; //seting the value to span from input box
+
+}else{
+    getEmail.classList.add("errorinbox");
+    errormessage.classList.remove("hide");
+}
+});
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+successBtn.addEventListener("click",function () {
+    location.reload();
+});
