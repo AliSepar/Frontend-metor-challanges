018a873bd379c85e0dde974001c1537599b8880e
AliSepar <alisabah.separ1@gmail.com>
2024-04-05 17:19:26 +0200

    first part of desgin is done working in js

---
+++ age-calculator-app-main/app.js
@@ -0,0 +1,20 @@
+
+let inputDay=document.getElementById('day');
+let inputMonth=document.getElementById('month');
+let inputYear=document.getElementById('year');
+
+let btn = document.querySelector('.btn');
+
+btn.addEventListener("click",function () {
+    if(isNaN(inputDay.value) && isNaN(inputMonth.value)&& isNaN(inputYear.value)){
+       
+            // not number error
+    }else{
+
+
+
+
+
+    }
+});
+
